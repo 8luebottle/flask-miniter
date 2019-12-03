89a9138f090fabdfbc7efd49b16f059e0493e576
8luebottle <itbiz.irs@gmail.com>
2019-12-04 00:46:10 +0900

    DECORATOR : pytest.fixture

--- miniter/test_endpoints.py
+++ miniter/test_endpoints.py
@@ -8,3 +8,10 @@ from sqlalchemy import create_engine, text
 
 database = create_engine(config.text_config['DB_URL'], encoding = 'UTF-8', max_overflow = 0)
 
+@pytest.fixture
+def api():
+    app = create_app(config.test_config)
+    app.config['TEST'] = True
+    api = app.test_client()
+
+    return api
