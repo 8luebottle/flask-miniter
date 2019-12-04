4600b451b6dac51f281bcaf946a6652063dcba34
8luebottle <itbiz.irs@gmail.com>
2019-12-04 11:58:59 +0900

    UNITTEST : LOGIN

--- miniter/test_endpoints.py
+++ miniter/test_endpoints.py
@@ -15,3 +15,37 @@ def api():
     api = app.test_client()
 
     return api
+
+
+def test_tweet(api):
+    # Create New Test User
+    new_user = {
+        'email'   : 'baby_tiger2019@gmail.com'
+        'passowrd': 'babytiger2019baby'
+        'name'    : 'Baby Tiger'
+        'profile' : 'Tech Blogger'
+    }
+    resp = api.post(
+        '/sign-up',
+        data         = json.dumps(new_user),
+        content_type = 'application/jsson'
+    )
+    assert resp.status_code ==200
+
+    # GET the ID of the New User
+    resp_json   = json.loads(resp.data.decode('UTF-8'))
+    new_user_id = resp_json['id']
+
+    # LOG IN TEST
+    rest = api.post(
+        '/login',
+        data = json.dumps(
+            { 
+                'email'   : 'baby_tiger2019@gmail.com',
+                'password': 'babytiger2019baby'
+            }
+        ), 
+        content_type = 'application/json'
+    )
+    rest_json    = json.loads(resp.data.decode('UTF-8'))
+    access_token = resp_json['access_token']
